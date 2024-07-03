6d5a4b6b4bcf69420a290c75bd9ebf8c641ca037
jayanth1976 <118813078+jayanth1976@users.noreply.github.com>
2024-07-03 13:32:28 +0530

    add email col in user model

--- src/main/model/user.py
+++ src/main/model/user.py
@@ -7,6 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(250), nullable=False)
     username = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(100), nullable=False)
     branch =  db.Column(db.String(100), nullable=False)
